889069d8d2ad8bc4c5e24884955162428645ed60
Amanda <Amanda@172.29.24.252>
2017-10-21 17:23:03 -0200

    todo initializers

---
+++ OCA-JP8/src/chapter4/Initializers.java
@@ -0,0 +1,14 @@
+package chapter4;
+
+public class Initializers {
+	// instance vars and initializers
+	private String name = "Torchie";
+	{ System.out.println(name); }
+	//
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
