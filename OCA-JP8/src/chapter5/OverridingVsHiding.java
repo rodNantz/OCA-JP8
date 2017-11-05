c16e16896e2f27cd53ab2f8e133847946e0e7828
Amanda <Amanda@172.29.7.4>
2017-11-05 14:01:48 -0200

    sdiahdubasudh

--- OCA-JP8/src/chapter5/OverridingVsHiding.java
+++ OCA-JP8/src/chapter5/OverridingVsHiding.java
@@ -1,18 +1,30 @@
 package chapter5;
 
-public class OverridingVsHiding { }
+public class OverridingVsHiding { 
+	public static void main(String[] args) {
+		ShadowVoldemort strendTingue = new ShadowVoldemort();
+		strendTingue.move();				// prints 'Wooof'
+		strendTingue = new Demogorgon();	
+		strendTingue.move();				// prints 'Wraah' (overriden method)
+		// Child reference cannot point to parent object (without explicit casting)
+		// Demogorgon dg = new ShadowVoldemort();
+	}
+}
+
+
+
 
 /*
  * INSTANCE
  */
 
-class ShadowMonster {
+class ShadowVoldemort {
 	protected void move(){
 		System.out.println("Wooooooooooofff");
 	}
 }
 
-class Demogorgon extends ShadowMonster {
+class Demogorgon extends ShadowVoldemort {
 	/* Good usage of overriding methods
 	 */
 	protected void move(){
@@ -20,12 +32,13 @@ class Demogorgon extends ShadowMonster {
 	}
 }
 
-class Will extends ShadowMonster {
-	/* A static method cannot HIDE an instance method
+class Will extends ShadowVoldemort {
+	/* A static method cannot HIDE an instance method (doesn't compile)
 	 */
+	/*
 	protected static void move(){
 		System.out.println("Can't move, It hurts everywhere");
-	}
+	} */
 }
 
 /*
