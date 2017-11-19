b486d9838bfd83db3d490069d3440fcaca02a024
rodtwo <rodtwo@hotmail.com>
2017-11-19 00:10:07 -0200

    solvin' merge

--- OCA-JP8/src/chapter2/OtherOperators.java
+++ OCA-JP8/src/chapter2/OtherOperators.java
@@ -19,7 +19,7 @@ public class OtherOperators {
 				System.out.println(" & ");
 			System.out.println("Not safe! (will not get here)");
 		} catch (NullPointerException npe) {
-			System.err.println("operator & " + npe);
+			System.err.print("operator & "); npe.printStackTrace();
 		}
 		
 		/* short-circuit / conditional operators:
@@ -67,8 +67,65 @@ public class OtherOperators {
 		
 		int m = 15 % 10;
 		System.out.println("m = 15 % 10	 	// m value is "+ m);
-		
 		m %= 3;
 		System.out.println("m %= 3  		// m = 5 % 3 which is "+ m);
+		// also with floating-point types
+		float f = 5.5f % 3;
+		System.out.println("5.5f % 3 == "+ f);
+
+		
+		/* OPERATOR TRICKS
+		 * 1. Strings: Do you remember that when one of the operands is a String, 
+		 * the + has operator has different meaning, right? 
+		 */
+		String str = "I'm ";
+		boolean b = true;
+		str += b;
+		System.out.println(str);
+		// And using = would NOT compile. 
+		// str = b;	
+		
+		/* 2. char: is a tricky primitive, pay attention to it.
+		 */
+		char c = 'c';
+		c += 1;
+		System.out.println(c);
+		c *= new Integer(2);
+		System.out.println(c);	
+		/* And again, = would not compile without explicit casting, because it's a char. 
+		 * Review 'Numeric Promotion'. 	*/
+		int i = 1;
+		// c = i;
+		
+		
+		/* EXTRA: Bitwise operators
+		 * You can also use bitwise operators with integral types, which
+		 * will result on an integral return 
+		 */
+		System.out.println("bw extra:");
+		int op1 = 60;
+		int op2 = 13;
+		System.out.println(op1 +" == \n\t"+ intToBinaryStrAddZeroes(op1) );
+		System.out.println(op2 +" == \n\t"+ intToBinaryStrAddZeroes(op2) );
+		int bwAnd = op1 & op2;
+		System.out.println(op1 +" & "+ op2 + " == \n\t"+ 
+							bwAnd + " == \n\t" + intToBinaryStrAddZeroes(bwAnd) );
+		
 	}
+	
+	public static String intToBinaryStrAddZeroes(int op){
+		String binaryStr = Integer.toBinaryString(op);
+		int fillin0s = 16 - binaryStr.length();
+		binaryStr = stringMultiply("0", fillin0s) + binaryStr;
+		return binaryStr;
+	}
+	
+	public static String stringMultiply(String s, int n){
+	    StringBuilder sb = new StringBuilder();
+	    for(int i = 0; i < n; i++){
+	        sb.append(s);
+	    }
+	    return sb.toString();
+	}
+	
 }
